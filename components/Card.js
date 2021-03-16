09157cad337ea73ecea42c2ef77294d6362ec7ee
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-03-16 11:37:42 +0500

    Fix responsivness for Card

--- components/Card.js
+++ components/Card.js
@@ -2,61 +2,52 @@ import React from "react"
 import {
   Card,
   CardContent,
+  Grid,
   Typography,
   makeStyles,
   CardActionArea,
   CardMedia,
-  Container,
-  CardActions,
 } from "@material-ui/core"
 import Badge from "react-simple-badges"
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     height: "100%",
   },
-  responsive: {
-    width: "100%",
-    height: "70%",
-  },
   media: {
     height: 280,
-    width: 540,
     marginTop: 5,
     marginLeft: 5,
+  },
+  badge: {
+    marginRight: 5,
   },
 }))
 
 const SmallCard = (props) => {
-  const classes = useStyles()
+  const classes = useStyles(props)
 
   return (
-    <Container maxWidth="sm">
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image={props.image}
-            onClick={(e) => {
-              console.log(e)
-            }}
-          />
-        </CardActionArea>
-        <CardContent>
-          <Typography color="textSecondary" variant="h6">
-            {props.title}
-          </Typography>
-          <Typography color="textPrimary" paragraph>
-            {props.description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          {props.tech.map((technology) => {
-            return <Badge name={technology} />
-          })}
-        </CardActions>
-      </Card>
-    </Container>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia className={classes.media} image={props.image} />
+      </CardActionArea>
+      <CardContent>
+        <Grid container justify="space-between" spacing={3}>
+          <Grid item>
+            <Typography color="textSecondary" gutterBottom variant="h6">
+              {props.title}
+            </Typography>
+            <Typography color="textPrimary" paragraph>
+              {props.description}
+            </Typography>
+            {props.tech.map((technology) => {
+              return <Badge name={technology} className={classes.badge} />
+            })}
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   )
 }
 
